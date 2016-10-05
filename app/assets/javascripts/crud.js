4321275972715a0786d1e22a533173302cca41cd
David Tinianow <david.tinianow@gmail.com>
2016-10-05 14:40:35 -0600

    Add ability to edit an idea title or body by hitting enter

--- app/assets/javascripts/crud.js
+++ app/assets/javascripts/crud.js
@@ -73,41 +73,46 @@ function deleteIdea() {
   })
 }
 
-function editIdea() {
-  $('#ideas-table').on('blur', '.idea-title', function(){
-    var newTitle = $(this).text();
-    var $idea = $(this).closest('tr');
-
-    $.ajax({
-      url: '/api/v1/ideas/' + $idea.data('id'),
-      type: 'put',
-      data: { idea: { title: newTitle } }
-    }).fail(handleError)
-  })
-}
-
 function editIdeaTitle() {
-  $('#ideas-table').on('blur', '.idea-title', function(){
-    var newTitle = $(this).text();
-    var $idea = $(this).closest('tr');
-
-    $.ajax({
-      url: '/api/v1/ideas/' + $idea.data('id'),
-      type: 'put',
-      data: { idea: { title: newTitle } }
-    }).fail(handleError)
-  })
+  $('#ideas-table').on('blur', '.idea-title', saveIdeaTitle)
+  $('#ideas-table').on('keydown', '.idea-title', function(e) {
+      if(e.keyCode == 13)
+      {
+          e.preventDefault();
+          $(this).blur();
+      }
+    })
 }
 
 function editIdeaBody() {
-  $('#ideas-table').on('blur', '.idea-body', function(){
-    var newBody = $(this).text();
-    var $idea = $(this).closest('tr');
+  $('#ideas-table').on('blur', '.idea-body', saveIdeaBody)
+  $('#ideas-table').on('keydown', '.idea-body', function(e) {
+      if(e.keyCode == 13)
+      {
+          e.preventDefault();
+          $(this).blur();
+      }
+    })
+}
 
-    $.ajax({
-      url: '/api/v1/ideas/' + $idea.data('id'),
-      type: 'put',
-      data: { idea: { body: newBody } }
-    }).fail(handleError)
-  })
+function saveIdeaBody() {
+  var newBody = $(this).text();
+  var $idea = $(this).closest('tr');
+
+  $.ajax({
+    url: '/api/v1/ideas/' + $idea.data('id'),
+    type: 'put',
+    data: { idea: { body: newBody } }
+  }).fail(handleError)
+}
+
+function saveIdeaTitle() {
+  var newTitle = $(this).text();
+  var $idea = $(this).closest('tr');
+
+  $.ajax({
+    url: '/api/v1/ideas/' + $idea.data('id'),
+    type: 'put',
+    data: { idea: { title: newTitle } }
+  }).fail(handleError)
 }
