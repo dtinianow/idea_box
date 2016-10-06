f859354003cb4552840b6cd3024dacb45924226b
David Tinianow <david.tinianow@gmail.com>
2016-10-06 11:24:16 -0600

    Write tests for updating and destroying ideas

--- spec/api/v1/ideas_controller_spec.rb
+++ spec/api/v1/ideas_controller_spec.rb
@@ -3,28 +3,31 @@ require 'rails_helper'
 describe 'Ideas' do
   context "#Index" do
     it 'can return all ideas in reverse order' do
+
+      idea = create(:idea, title: 'Hi', body: 'Hello', created_at: Date.yesterday)
       idea = create(:idea)
 
       get '/api/v1/ideas'
 
       ideas = JSON.parse(response.body)
-      idea = ideas.first
+      idea = ideas.last
 
       expect(response).to be_success
-      expect(ideas.length).to eq(1)
+      expect(ideas.length).to eq(2)
       expect(ideas.class).to eq(Array)
       expect(idea.class).to eq(Hash)
-      expect(idea['title']).to eq('Alphabet')
-      expect(idea['body']).to eq('a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z ')
+      expect(idea['title']).to eq('Hi')
+      expect(idea['body']).to eq('Hello')
       expect(idea['quality']).to eq('swill')
     end
   end
 
   context "#Create" do
     it 'can create a new idea' do
+
       params = {
-          title: "Hey",
-          body: "Hello"
+        title: "Hey",
+        body: "Hello"
       }
 
       post '/api/v1/ideas', idea: params
@@ -38,42 +41,36 @@ describe 'Ideas' do
       expect(idea['quality']).to eq('swill')
     end
   end
-end
 
-#
-#   it 'can return JSON data on a specific item' do
-#     get '/api/v1/items/2'
-#
-#     item = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(item.class).to eq(Hash)
-#     expect(item['id']).to eq(2)
-#     expect(item['name']).to eq('Robotron')
-#     expect(item['description']).to eq('Boop boop beep')
-#   end
-#
-#   it 'can create an item' do
-#     item_params = { name: "Brobot",
-#                     description: "The broiest robot",
-#                     image_url: 'http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200'
-#     }
-#
-#     post "/api/v1/items", item: item_params
-#     item = Item.last
-#
-#     expect(response.status).to eq 201
-#     expect(item['name']).to eq 'Brobot'
-#     expect(item['description']).to eq 'The broiest robot'
-#     expect(item['image_url']).to eq 'http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200'
-#   end
-#
-#
-#   it 'can delete an item' do
-#     item = items(:one)
-#
-#     delete "/api/v1/items/#{item.id}"
-#
-#     expect(response.status).to eq 204
-#   end
-# end
+  context "#Update" do
+    it 'can update an idea' do
+
+      idea = create(:idea)
+      params = {
+        title: "Hey",
+        body: "Hello",
+        quality: 'genius'
+      }
+
+      put "/api/v1/ideas/#{idea.id}", idea: params
+
+      idea = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(idea.class).to eq(Hash)
+      expect(idea['title']).to eq('Hey')
+      expect(idea['body']).to eq('Hello')
+      expect(idea['quality']).to eq('genius')
+    end
+  end
+
+  context '#Destroy' do
+    it 'can delete an idea' do
+      idea = create(:idea)
+
+      delete "/api/v1/ideas/#{idea.id}"
+
+      expect(response.status).to eq 204
+    end
+  end
+end
