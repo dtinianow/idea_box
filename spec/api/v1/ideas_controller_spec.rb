68198b5ae40aa7793848c75aa93e0e54d7bdceb5
David Tinianow <david.tinianow@gmail.com>
2016-10-06 11:07:37 -0600

    Write controller test for api v1 ideas index

---
+++ spec/api/v1/ideas_controller_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+describe 'Ideas' do
+
+  context "#Index" do
+    it 'returns JSON data on all ideas in reverse order' do
+      idea = create(:idea)
+
+      get '/api/v1/ideas'
+
+      ideas = JSON.parse(response.body)
+      idea = ideas.first
+
+      expect(response).to be_success
+      expect(ideas.length).to eq(1)
+      expect(ideas.class).to eq(Array)
+      expect(idea.class).to eq(Hash)
+      expect(idea['title']).to eq('Alphabet')
+      expect(idea['body']).to eq('a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z ')
+      expect(idea['quality']).to eq('swill')
+    end
+  end
+end
+#
+# require 'rails_helper'
+#
+# describe "Items" do
+#   fixtures :items
+#
+#   it 'can return JSON data on all items' do
+#     get '/api/v1/items'
+#
+#     items = JSON.parse(response.body)
+#     item = items.first
+#
+#     expect(response).to be_success
+#     expect(items.length).to eq(2)
+#     expect(items.class).to eq(Array)
+#     expect(item.class).to eq(Hash)
+#     expect(item['id']).to eq(1)
+#     expect(item['name']).to eq("Robot")
+#     expect(item['description']).to eq("Beep boop")
+#   end
+#
+#   it 'can return JSON data on a specific item' do
+#     get '/api/v1/items/2'
+#
+#     item = JSON.parse(response.body)
+#
+#     expect(response).to be_success
+#     expect(item.class).to eq(Hash)
+#     expect(item['id']).to eq(2)
+#     expect(item['name']).to eq('Robotron')
+#     expect(item['description']).to eq('Boop boop beep')
+#   end
+#
+#   it 'can create an item' do
+#     item_params = { name: "Brobot",
+#                     description: "The broiest robot",
+#                     image_url: 'http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200'
+#     }
+#
+#     post "/api/v1/items", item: item_params
+#     item = Item.last
+#
+#     expect(response.status).to eq 201
+#     expect(item['name']).to eq 'Brobot'
+#     expect(item['description']).to eq 'The broiest robot'
+#     expect(item['image_url']).to eq 'http://robohash.org/1.png?set=set2&bgset=bg1&size=200x200'
+#   end
+#
+#
+#   it 'can delete an item' do
+#     item = items(:one)
+#
+#     delete "/api/v1/items/#{item.id}"
+#
+#     expect(response.status).to eq 204
+#   end
+# end
--- spec/features/user_can_see_ideas_spec.rb
+++ spec/features/user_can_see_ideas_spec.rb
@@ -3,25 +3,23 @@ require 'rails_helper'
 describe "User can see ideas", :type => :feature, :js => true do
   scenario 'they visit the root path and see title, description, and quality' do
 
-    backwards_idea = create(:backwards_idea)
-    alphabet_idea  = create(:alphabet_idea)
+    idea = create(:idea)
     ideas = Idea.order(created_at: :desc)
     alphabet_truncated_body = 'a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v'
 
     visit root_path
 
-    expect(ideas.first).to eq alphabet_idea
-    expect(ideas.last).to eq backwards_idea
-    expect(alphabet_idea.quality).to eq 'swill'
+    expect(ideas.first).to eq idea
+    expect(idea.quality).to eq 'swill'
     expect(page).to have_content 'Idea Box'
 
     wait_for_ajax
-    find("#idea-#{alphabet_idea.id}")
+    find("#idea-#{idea.id}")
 
-    within("#idea-#{alphabet_idea.id}") do
-      expect(page).to have_content alphabet_idea.title
+    within("#idea-#{idea.id}") do
+      expect(page).to have_content idea.title
       expect(page).to have_content alphabet_truncated_body
-      expect(page).to have_content alphabet_idea.quality
+      expect(page).to have_content idea.quality
     end
   end
 end
